bb05035f8fc61e04f17edc370e1490c9d806c726
vaibhavDharmik1 <vaibhavdharmik55.com>
2021-12-21 22:25:34 -1100

    Play the game with 2 player

---
+++ SnakeLadderUC_7.java
@@ -0,0 +1,31 @@
+public class SnakeLadderUC_7 {
+	public static void main(String[] args) {
+		int position=0;
+		int count=0;
+		while(position<=100) {
+			count++;
+		int snake= (int) Math.floor(Math.random() * 10) %3;
+		int check =(int) Math.floor(Math.random() * 10) %7 ;
+		if(snake==1) {
+			position =position + check;
+			if(position>100) {
+				position = position - check;
+
+			}
+		}
+		else if(snake==2) {
+			position =position - check;
+			if(position<0) {
+				position=0;
+			}
+		}
+		else {
+			//no play
+			count=count-1;
+			
+		}
+		System.out.println("No of times dice roll is : " + count);
+
+		System.out.println("current postion  is: " + position);
+	}}
+}
